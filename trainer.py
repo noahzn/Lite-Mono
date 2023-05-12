81073d4876c5ea895d67db912d4dcf4cf1fca93a
noahzn <18817353383@qq.com>
2023-05-12 09:14:44 +0200

    fix training image size 1024x320

--- trainer.py
+++ trainer.py
@@ -56,7 +56,8 @@ class Trainer:
             self.opt.frame_ids.append("s")
 
         self.models["encoder"] = networks.LiteMono(model=self.opt.model,
-                                                   drop_path_rate=self.opt.drop_path)
+                                                   drop_path_rate=self.opt.drop_path,
+                                                   width=self.opt.width, height=self.opt.height)
 
         self.models["encoder"].to(self.device)
         self.parameters_to_train += list(self.models["encoder"].parameters())
